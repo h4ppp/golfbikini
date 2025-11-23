e4a0924f26232b2f6663bc9e323b7ab0dca62211
Oleksii <alexey8medvedev@gmail.com>
2025-11-23 11:56:03 +0200

    2311

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,36 @@ export const metadata = {
     title: "Bikini Golf",
     description:
         "Golf Has Never Looked Like This,Tropical courses. Bikini-clad golfers with serious skills. Each episode delivers beauty, competition, and unforgettable moments. Welcome to Bikini Golf — where ...",
+
     icons: {
         icon: "/img/favicon.svg",
         shortcut: "/img/favicon.svg",
         apple: "/img/favicon.svg",
+    },
+
+    openGraph: {
+        title: "Bikini Golf",
+        description:
+            "Golf Has Never Looked Like This,Tropical courses. Bikini-clad golfers with serious skills. Each episode delivers beauty, competition, and unforgettable moments. Welcome to Bikini Golf — where ...",
+        url: "https://www.bikinigolfofficial.com",
+        siteName: "Bikini Golf",
+        type: "website",
+        images: [
+            {
+                url: "/img/og.png",
+                width: 1200,
+                height: 630,
+                alt: "Bikini Golf Preview",
+            }
+        ],
+    },
+
+    twitter: {
+        card: "summary_large_image",
+        title: "Bikini Golf",
+        description:
+            "Golf Has Never Looked Like This,Tropical courses. Bikini-clad golfers with serious skills. Each episode delivers beauty, competition, and unforgettable moments. Welcome to Bikini Golf — where ...",
+        images: ["/img/og.png"],
     },
 };
 
